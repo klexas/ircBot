4372dd1e087a6bd1266f9b30884d4f827059c477
Adam Robins <iam@adamrobins.com>
2022-03-25 00:05:16 +0100

    extension for bo prevention

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 var irc = require("irc-connect");
 var helper = require('./helper/lib.js');
+var dataStore = require('./datastore');
+
 // TODO: Add to configuration file. 
 var ircOptions = {
-    channels: ["#testChannel"],
+    channels: ["#hd-torrents", "#tadass"],
     // Defaults to 6667
     port: 6667,
-    pass: "password",
+    pass: "sakura123$",
     secure: false,
-    nick: 'Nick',
+    nick: 'slut',
     realname: 'BotNickRealName',
     ident: 'Ident'
 }
 
-var client = irc.connect('irc.freenode.net', ircOptions)
+irc.connect('irc.digitalirc.org', ircOptions)
     //include some plugins 
     .use(irc.pong, irc.names, irc.motd)
     //fires when the servers sends the welcome message (RPL_WELCOME) 
     .on('welcome', function (msg) {
-        console.log(msg);
+        // console.log(msg);
         this.nick(ircOptions.nick, ircOptions.pass, function (err) {
             console.log('There was a problem setting your NICK:', err);
         });
     })
     //fires after the server confirms password 
-    .on('identified', function (nick) {
-        // this.send('JOIN #pogrindis');
-    })
+    // .on('identified', function (nick) {
+    //     this.send('JOIN #hd-torrents hdtskeyforirc2021');
+    // })
     //fires only when YOUR nick changes 
     .on('nick', function (nick) {
         console.log('Your nick is now:', nick);
@@ -37,35 +39,42 @@ var client = irc.connect('irc.freenode.net', ircOptions)
         console.log(event.nick, 'joined');
     })
     .on('MODE', function (mode) {
-        console.log("MODE CHANGED");
         // If registered
         if (mode.params[1] == '+r') {
             console.log("Identified nick");
+            // HDTS Passkey addition
+            // this.send('JOIN #hd-torrents hdtskeyforirc2021');
+
             // this.send('JOIN #tester');
             console.log(helper);
             helper.joinRooms(ircOptions.channels, this);
+            this.send('JOIN ' + ircOptions.channels[0]);
         }
         console.log(mode);
     })
     .on('PRIVMSG', function (event) {
         var params = event.params;
-        console.log("MESSAGE RECIEVED AND PARSED");
-        if (event.nick == 'adam') {
-            // this.send(params[0] );
-            // this.send('PRIVMSG '+params[0]+' :Success debug event!');
-            console.log(params);
-            // Check the message. 
-            if (params[1][0] == "!") {
-                console.log("We have a command");
-                var response = helper.processCommand(params[1], event.nick);
-                if (response == "Quit") {
-                    this.send('QUIT : B');
-                }
-                this.send('PRIVMSG ' + params[0] + ' :' + response);
+        
+        // if we have a cmd - process it
+        if (params[1][0] == ".") {
+            console.log("We have a command");
+            var response = helper.processCommand(params, event.nick, this);
+            if (response == "Quit") {
+                this.send('QUIT : Bye, off to service tadas ballz');
             }
+            if(response == 'INVITE_OK'){
+                console.log('Inviting tadas');
+                console.log(event);
+
+                this.send('INVITE', 'tadasbot', 'tadas', 'invited');
+            }
+            this.send('PRIVMSG ' + params[0] + ' :' + response);
         }
 
-        console.log('message from: ' + event.nick, 'to: ' + params[0], params[1]);
+        // Otherwise check for messages. 
+        dataStore.getMessage(event.nick);
+
+        // console.log('message from: ' + event.nick, 'to: ' + params[0], params[1]);
     })
     //from the `names` plugin. 
     .on('names', function (cname, names) {
@@ -73,12 +82,23 @@ var client = irc.connect('irc.freenode.net', ircOptions)
     })
     //from the `motd` plugin. 
     .on('motd', function (event) {
-        console.log(this.motd);
-        console.log(this.support);
+        // console.log(this.motd);
+        // console.log(this.support);
     })
     .on('message', function (details) {
-        console.log(details);
+        console.log("PM RECIEVED 1 mode lowercase");
+        // console.log(details);
     })
     .on('MESSAGE', function (details) {
-        console.log(details);
+        console.log("PM RECIEVED");
+        // console.log(details);
+    })
+    .on('INVITE', function(details) {
+        // console.log(details);
+        // join params[1]
+        this.send('PRIVMSG', details.params[0], "Thanks for the invite!");
+        // Command: PRIVMSG
+        // Parameters: <receiver>{,<receiver>} <text to be sent>
+        this.send('JOIN ' + details.params[1]);
+        console.log("INVITE RECIEVED");
     });
